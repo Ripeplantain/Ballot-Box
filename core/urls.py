fe69af0a94cbde6eb48a23da146ee8f92ecf47d3
Ripeplantain <irvingmanny@gmail.com>
2023-02-25 12:31:43 +0000

    done with home page

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('home/',home_view, name='home'),
+    path('election/<int:id>',election_view, name='election'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,25 @@
 from django.shortcuts import render
+from .models import Election
 
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'core/home.html')
+    """
+    Home view
+    """
+
+    elections = Election.objects.all()
+
+    context = {'elections': elections}
+    return render(request, 'core/home.html',context)
+
+
+def election_view(request, id):
+    """
+    Election view
+    """
+
+    election = Election.objects.get(id=id)
+
+    context = {'election': election}
+    return render(request, 'core/election.html',context)
